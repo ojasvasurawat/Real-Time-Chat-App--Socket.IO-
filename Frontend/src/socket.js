a7a833c00af1351612365be7c9376d1583ebee1a
Ojasva Surawat <surawatojasva@gmail.com>
2026-02-08 14:24:54 +0530

    socket connection fixed

--- Frontend/src/socket.js
+++ Frontend/src/socket.js
@@ -1,6 +1,9 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3000/chat", {
+export const socket = io(
+  // "http://localhost:3000/chat",
+  "https://real-time-chat-app-socket-io-tcnh.onrender.com/chat"
+  , {
   transports: ["websocket"],
   auth:{
     token: localStorage.getItem('authorization')
